a3f5b2ba33aeab1290c96a62068ddf41cc8a95a6
André <andre.spanguero@gmail.com>
2021-10-11 15:46:02 -0300

    Update fix_names.sh

--- fix_names.sh
+++ fix_names.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-while IFS="," read -r name wrong_last_name last_name
+while IFS="," read -r id name wrong_last_name last_name
 do
-  echo "Fixing name: $name $last_name"
+  echo "Fixing name: $name $last_name for id $id"
   sleep 3
 done < <(tail -n +2 exemplo-screen.csv)
